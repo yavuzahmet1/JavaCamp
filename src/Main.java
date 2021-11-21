f2044a4621a30d87f2311228a6d88d1f8592cc00
Ahmet YAVUZ <1yavuzahmet@gmail.com>
2021-11-21 16:29:06 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Entities.abstracts.Entitiy;
+import Entities.concretes.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user=new User("Ahmet","YAVUZ","1yavuzahmet@gmail.com","123456",true);
+
+    }
+}
