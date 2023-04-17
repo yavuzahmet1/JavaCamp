8b336a6e59256fdf9e625aac6d0dc214d0c24e04
Coders club <1yavuzahmet@gmail.com>
2023-04-17 21:57:54 +0300

    resul added

---
+++ week7/northwind-demo/northwind-demo/src/main/java/kodlama/io/northwind/core/utilities/result/ErrorDataResult.java
@@ -0,0 +1,19 @@
+package kodlama.io.northwind.core.utilities.result;
+
+public class ErrorDataResult <T>extends DataResult{
+    public ErrorDataResult(Object data, String message) {
+        super(data, false, message);
+    }
+
+    public ErrorDataResult(T data) {
+        super(data, false);
+    }
+
+    public ErrorDataResult(String message) {
+        super(null, false, message);
+    }
+
+    public ErrorDataResult() {
+        super(null, false);
+    }
+}
---
+++ week7/northwind-demo/northwind-demo/src/main/java/kodlama/io/northwind/core/utilities/result/SuccessDataResult.java
@@ -0,0 +1,19 @@
+package kodlama.io.northwind.core.utilities.result;
+
+public class SuccessDataResult<T> extends DataResult {
+    public SuccessDataResult(Object data, String message) {
+        super(data, true, message);
+    }
+
+    public SuccessDataResult(T data) {
+        super(data, true);
+    }
+
+    public SuccessDataResult(String message) {
+        super(null, true, message);
+    }
+
+    public SuccessDataResult() {
+        super(null, true);
+    }
+}
