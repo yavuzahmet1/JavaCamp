7ad7a2c54ea8995dad2e9c5ee01121632e2fa403
Coders club <1yavuzahmet@gmail.com>
2023-04-29 09:42:14 +0300

    week10 basic javascript start

--- week10/camp_v2/src/app.js
+++ week10/camp_v2/src/app.js
@@ -1,1 +1,17 @@
 console.log("Merhaba Kodlama.io")
+let students={id:1,name:"Ahmet"}
+console.log(students)
+
+function save(){
+    console.log("saved !")
+}
+
+save();
+
+function saved(student){
+    console.log(student+" : saved !")
+}
+
+saved(students)
+
+///1.32 de kaldım
