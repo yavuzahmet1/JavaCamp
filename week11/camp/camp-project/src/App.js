40343c484c2e62637bc72c0a916759157c3577cc
Coders club <1yavuzahmet@gmail.com>
2023-05-01 11:47:37 +0300

    week11 react created

--- week11/camp/camp-project/src/App.js
+++ week11/camp/camp-project/src/App.js
@@ -1,25 +1,10 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Dashboard from './layouts/Dashboard'
-import 'semantic-ui-css/semantic.min.css'
-import { Container } from 'semantic-ui-react';
-import Navi from './layouts/Navi';
 
 function App() {
   return (
     <div className="App">
-      <Navi />
-      <Container className="main">
-        <Dashboard />
-      </Container>
-    </div>
-  );
-}
-
-export default App;
-
-
-/*     <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -32,4 +17,9 @@ export default App;
         >
           Learn React
         </a>
-      </header>*/
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- week11/camp/src/layouts/BrandServices.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export default class BrandService {
-    getBrands() {
-        
-        return axios.get('http://localhost:8082/api/models');
-
-    }
-}
