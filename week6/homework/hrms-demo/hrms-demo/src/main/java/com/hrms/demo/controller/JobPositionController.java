032724cc8e508193e7860b49649e677ff75a25f4
Coders club <1yavuzahmet@gmail.com>
2023-05-01 11:18:47 +0300

    week6 hrms->swagger

--- week6/homework/hrms-demo/hrms-demo/src/main/java/com/hrms/demo/controller/JobPositionController.java
+++ week6/homework/hrms-demo/hrms-demo/src/main/java/com/hrms/demo/controller/JobPositionController.java
@@ -19,6 +19,6 @@ public class JobPositionController {
 
     @GetMapping
     public List<JobPosition> getAll(){
-        return this.jobPositionService.getAllJob();
+        return jobPositionService.getAllJob();
     }
 }
--- week9/northwind-demo/northwind-demo/src/main/java/kodlama/io/northwind/NorthwindDemoApplication.java
+++ week9/northwind-demo/northwind-demo/src/main/java/kodlama/io/northwind/NorthwindDemoApplication.java
@@ -3,19 +3,15 @@ package kodlama.io.northwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-//@EnableSwagger2
+
 public class NorthwindDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindDemoApplication.class, args);
 	}
-	/*@Bean
-	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("kodlama.io.northwind"))
-				.build();
-	}*/
+
 
 }
