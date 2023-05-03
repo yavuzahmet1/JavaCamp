9efa36a7dad441f7e3994c573418848b3219907f
Coders club <1yavuzahmet@gmail.com>
2023-05-03 22:15:39 +0300

    week11 react->table and menu added

--- week11/camp/camp-project/src/layouts/Dashboard.jsx
+++ week11/camp/camp-project/src/layouts/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navi from './Navi'
 import Categories from './Categories'
 import ProductList from '../pages/ProductList'
 
@@ -8,7 +7,7 @@ import ProductList from '../pages/ProductList'
 export default function Dashboard() {
   return (
     <div>
-      <Navi/>
+      
       <Categories/>
       <ProductList/>
     
